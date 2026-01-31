c34e0c7b31ddcd02a6da47b0e0964f6ad9662b8c
bascom241 <135514268+bascom241@users.noreply.github.com>
2026-01-31 12:06:35 +0100

    Deployment urls

--- backend/server.js
+++ backend/server.js
@@ -13,14 +13,19 @@ import chatRouter from "./routes/chat.js"
 dotenv.config();
 app.use(express.json());
 
-app.options("*", cors());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174","https://9ja-taxes-9c4e.vercel.app","https://9ja-taxes-9c4e.vercel.app/", "https://www.9jataxes.com", "https://www.9jataxes.com/"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://9ja-taxes-9c4e.vercel.app",
+      "https://www.9jataxes.com",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
 );
+
 
 // Optional: parse URL-encoded data (from forms)
 app.use(express.urlencoded({ extended: true }));
