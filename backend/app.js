5fc91ccda628b98c64490b4072df449330267982
bascom241 <135514268+bascom241@users.noreply.github.com>
2026-01-31 12:02:56 +0100

    Deployment urls

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 import express from "express"
 const app = express();
+app.use((req, res, next) => {
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Origin", "https://www.9jataxes.com");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 
 export default app; 
